85d6eea9e672004c643b3dc75c510fcbd689d899
pheeno <kyatth@163.com>
2019-02-23 01:43:12 +0800

    vue extend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './button-group.vue'
 
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
 new Vue({
-  el:'#app',
-}) 
+  el: '#app'
+})
+// 单元测试
+{
+  const Constructor = Vue.extend(Button)
+  let btn = new Constructor({
+    propsData:{
+      icon:'settings'
+    }
+  })
+  btn.$mount('#test')
+}
