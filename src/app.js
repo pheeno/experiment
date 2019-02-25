e0a9ac33921e698b23822a65e0d9d9498ecfd320
pheeno <kyatth@163.com>
2019-02-25 21:04:22 +0800

    travis ci

--- src/app.js
+++ src/app.js
@@ -24,6 +24,8 @@ const expect = chai.expect
   let useElement = btn.$el.querySelector('use')
   let href = useElement.getAttribute('xlink:href')
   expect(href).to.eq('#i-settings')
+  btn.$el.remove()
+  btn.$destroy()
 }
 {
   const Constructor = Vue.extend(Button)
@@ -37,6 +39,8 @@ const expect = chai.expect
   let useELement = btn.$el.querySelector('use')
   let href = useELement.getAttribute('xlink:href')
   expect(href).to.eq('#i-loading')
+  btn.$el.remove()
+  btn.$destroy()
 }
 {
   const Constructor = Vue.extend(Button)
@@ -50,4 +54,22 @@ const expect = chai.expect
   let useELement = btn.$el.querySelector('use')
   let href = useELement.getAttribute('xlink:href')
   expect(href).to.eq('#i-loading')
+  btn.$el.remove()
+  btn.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  let btn = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  btn.$mount(div)
+  let svg = btn.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  btn.$el.remove()
+  btn.$destroy()
 }
